9068d05bc044bdce2db40bea2f8705bef923fb6e
cagatayy94 <mustafa.cagatay.yilmaz@gmail.com>
2021-05-21 18:38:12 +0300

    Implemented swagger

--- src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package kodlamaio.northwind.api.controllers;
 import kodlamaio.northwind.business.abstracts.ProductService;
 import kodlamaio.northwind.entities.concretes.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class ProductsController {
         this.productService = productService;
     }
 
-    @RequestMapping("/getAll")
+    @GetMapping("/getAll")
     public List<Product> getAll(){
         return this.productService.getAll();
     }
